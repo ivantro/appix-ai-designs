c34e6921cc06758b7124b016f2a69a3181919d91
Ivan Trotter <ivantrotter@gmail.com>
2025-05-05 13:10:24 -0400

    Testing a change in code

--- Ping.cs
+++ Ping.cs
@@ -18,7 +18,7 @@ namespace com.appix.ai.design
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            return new OkObjectResult("Welcome to Azure Functions2!");
         }
     }
 }
